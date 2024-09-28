49abd436b43717d2423007facdbb327c92c444d3
Antoine <ant.delalande@gmail.com>
2024-09-28 09:28:20 +0200

    server setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello From Typescript")
+import express from "express";
+import {Request, Response, NextFunction} from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+})
+
+app.listen(port, () => {
+    console.log(`The Good Corner listening on port ${port}`)
+})
