94ec28db7513e19d39682d490cc98d9f6aa4002f
Antoine <ant.delalande@gmail.com>
2024-09-30 11:41:13 +0200

    added sample ads

--- frontend/src/components/RecentAds/RecentAds.tsx
+++ frontend/src/components/RecentAds/RecentAds.tsx
@@ -3,56 +3,57 @@ import AdCard from "../AdCard/AdCard";
 
 
 export default function RecentAds() {
-    return (<main className="main-content">
+
+    const ads = [
+        {
+            imgUrl: "/images/table.webp",
+            title: "Table",
+            link: "/ads/table",
+            price: "120 €"
+        },
+        {
+            imgUrl: "/images/dame-jeanne.webp",
+            title: "Dame-Jeanne",
+            link: "/ads/dame-jeanne",
+            price: "75 €"
+        },
+        {
+            imgUrl: "/images/vide-poche.webp",
+            title: "Vide-Poche",
+            link: "/ads/vide-poche",
+            price: "4 €"
+        },
+        {
+            imgUrl: "/images/vaisselier.webp",
+            title: "Vaisselier",
+            link: "/ads/vaisselier",
+            price: "900 €"
+        },
+        {
+            imgUrl: "/images/bougie.webp",
+            title: "Bougie",
+            link: "/ads/bougie",
+            price: "8 €"
+        },
+        {
+            imgUrl: "/images/porte-magazine.webp",
+            title: "Porte-Magazine",
+            link: "/ads/porte-magazine",
+            price: "45 €"
+        }
+        
+    ]
+
+
+    return (
+    <main className="main-content">
       <h2>Annonces récentes</h2>
       <section className="recent-ads">
-        <AdCard link="/ads/table" imgUrl="/images/table.webp" price="120 €" title="Table"/>
+        {ads.map((ad) => (
+            <AdCard link={ad.link} imgUrl={ad.imgUrl} price={ad.price} title={ad.title} key={ad.title}/>
+        ))}
         
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/dame-jeanne">
-            <img className="ad-card-image" src="/images/dame-jeanne.webp" alt=""/>
-            <div className="ad-card-text">
-              <div className="ad-card-title">Dame-jeanne</div>
-              <div className="ad-card-price">75 €</div>
-            </div>
-          </a>
-        </div>
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/vide-poche">
-            <img className="ad-card-image" src="/images/vide-poche.webp" alt=""/>
-            <div className="ad-card-text">
-              <div className="ad-card-title">Vide-poche</div>
-              <div className="ad-card-price">4 €</div>
-            </div>
-          </a>
-        </div>
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/vaisselier">
-            <img className="ad-card-image" src="/images/vaisselier.webp" alt=""/>
-            <div className="ad-card-text">
-              <div className="ad-card-title">Vaisselier</div>
-              <div className="ad-card-price">900 €</div>
-            </div>
-          </a>
-        </div>
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/bougie">
-            <img className="ad-card-image" src="/images/bougie.webp" alt=""/>
-            <div className="ad-card-text">
-              <div className="ad-card-title">Bougie</div>
-              <div className="ad-card-price">8 €</div>
-            </div>
-          </a>
-        </div>
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/porte-magazine">
-            <img className="ad-card-image" src="/images/porte-magazine.webp" alt=""/>
-            <div className="ad-card-text">
-              <div className="ad-card-title">Porte-magazine</div>
-              <div className="ad-card-price">45 €</div>
-            </div>
-          </a>
-        </div>
       </section>
-    </main>)
+    </main>
+    )
 }
